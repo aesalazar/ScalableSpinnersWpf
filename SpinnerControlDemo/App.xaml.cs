8187392341160b3c4863118fc0f81d3b29304f0b
Ernie Salazar <salazare@verizon.net>
2017-01-17 17:35:40 -0500

    Added attached properties GridRowsProperty and GridColumnsProperty to allow setting row and column defs in styles.

--- SpinnerControlDemo/App.xaml.cs
+++ SpinnerControlDemo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SpinnerControlDemo
 {
@@ -13,5 +14,134 @@ namespace SpinnerControlDemo
     /// </summary>
     public partial class App : Application
     {
+        //*******
+        //Create attached properties that allow for setting grid rows and columns in styles
+        //*******
+
+        /// <summary>
+        /// Allows for setting grid rows in a style using a csv string of lengths.
+        /// <example>Auto,1,*,2*,1.5*</example>
+        /// </summary>
+        public static readonly DependencyProperty GridRowsProperty = DependencyProperty.RegisterAttached(
+            "GridRows"
+            , typeof(string)
+            , typeof(App)
+            , new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsMeasure, GridRowsPropertyChanged));
+
+        private static void GridRowsPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = dependencyObject as Grid;
+            if (sender == null)
+                throw new ArgumentException($"GridRows property could not get the Grid object from type '{dependencyObject.GetType()}'");
+
+            DefineGridRows(sender);
+        }
+
+        public static void SetGridRows(DependencyObject element, string value)
+        {
+            element.SetValue(GridRowsProperty, value);
+        }
+        public static string GetGridRows(DependencyObject element)
+        {
+            return (string) element.GetValue(GridRowsProperty);
+        }
+
+        private static void DefineGridRows(Grid grid)
+        {
+            var rows = GetGridRows(grid).Split(Convert.ToChar(","));
+            grid.RowDefinitions.Clear();
+
+            foreach (var row in rows)
+            {
+                switch (row.Trim().ToLower())
+                {
+                    case "auto":
+                        grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                        break;
+
+                    case "*":
+                        grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                        break;
+
+                    default:
+                        double length;
+                        var isStarred = row.EndsWith("*");
+                        var isNumeric = double.TryParse(isStarred ? row.TrimEnd('*') : row, out length);
+
+                        if (!isNumeric)
+                            throw new ArgumentException("GridRows property must be a csv like: 'Auto,1,*,2*,1.5*'");
+
+                        grid.RowDefinitions.Add(new RowDefinition
+                        {
+                            Height = new GridLength(length, isStarred ? GridUnitType.Star : GridUnitType.Pixel)
+                        });
+
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Allows for setting grid columns in a style using a csv string of lengths.
+        /// <example>Auto,*,2*,1.5*</example>
+        /// </summary>
+        public static readonly DependencyProperty GridColumnsProperty = DependencyProperty.RegisterAttached(
+            "GridColumns"
+            , typeof(string)
+            , typeof(App)
+            , new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsMeasure, GridColumnsPropertyChanged));
+
+        private static void GridColumnsPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = dependencyObject as Grid;
+            if (sender == null)
+                throw new ArgumentException($"GridColumns property could not get the Grid object from type '{dependencyObject.GetType()}'");
+
+            DefineGridColumns(sender);
+        }
+
+        public static void SetGridColumns(DependencyObject element, string value)
+        {
+            element.SetValue(GridColumnsProperty, value);
+        }
+        public static string GetGridColumns(DependencyObject element)
+        {
+            return (string)element.GetValue(GridColumnsProperty);
+        }
+
+        private static void DefineGridColumns(Grid grid)
+        {
+            var rows = GetGridColumns(grid).Split(Convert.ToChar(","));
+            grid.ColumnDefinitions.Clear();
+
+            foreach (var row in rows)
+            {
+                switch (row.Trim().ToLower())
+                {
+                    case "auto":
+                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) });
+                        break;
+
+                    case "*":
+                        grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                        break;
+
+                    default:
+                        double length;
+                        var isStarred = row.EndsWith("*");
+                        var isNumeric = double.TryParse(isStarred ? row.TrimEnd('*') : row, out length);
+
+                        if (!isNumeric)
+                            throw new ArgumentException("GridColumns property must be a csv like: 'Auto,1,*,2*,1.5*'");
+
+                        grid.ColumnDefinitions.Add(new ColumnDefinition
+                        {
+                            Width = new GridLength(length, isStarred ? GridUnitType.Star : GridUnitType.Pixel)
+                        });
+
+                        break;
+                }
+            }
+        }
     }
 }
